1bdd2343bc6744a43f83356f13a5f1aac7b2e97d
nandhakumar-io <nandhakumarsys@gmail.com>
2026-03-26 10:59:09 +0530

    Move to flake structure

---
+++ hosts/kenpachi/configuration.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }: {
+	imports = [
+		./hardware-configuration.nix
+
+		../../modules/system.nix
+		../../modules/services.nix
+		../../modules/packages.nix
+		../../modules/programs.nix
+		../../modules/users.nix
+		../../modules/fonts.nix
+		../../modules/networking.nix
+		];
