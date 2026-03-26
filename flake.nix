9ae3a21f2aadcc4ae97aa633a16eb3df6d749b2c
nandhakumar-io <nandhakumarsys@gmail.com>
2026-03-26 11:14:56 +0530

    Fixed Some  Syntax erros

--- flake.nix
+++ flake.nix
@@ -3,18 +3,16 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    }
+    };
 
-    outputs = { self , nixpkgs}: {
-        nixConfigurations.AsusVivobookS15 = {
-            nixpkgs.lib.nixosSystem {
+    outputs = { self, nixpkgs}: {
+        nixConfigurations.AsusVivobookS15 = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
                 
                 modules = [
-                    ./configuration.nix 
+                    ./hosts/kenpachi/configuration.nix
                 ];
 
-            }
-        }
+            };
+        };
     }
-}
